7239445c0f9987996aa23aebebf14e9159ce2152
Shalu Kumari <16433381+shablcuShaluRai@users.noreply.github.com>
2023-06-22 08:57:02 +0530

    Added filter function and prototype using js

---
+++ src/jspollyfil/arrFilter.js
@@ -0,0 +1,53 @@
+const arr = [2, 3, 4, 5, 6, 7, 8];
+
+const getEven = (ar) => ar % 2 === 0;
+
+const filteredEle = arr.filter(getEven);
+
+console.log("filteredEle", filteredEle);
+
+// filter protoype using for loop
+
+// Array.prototype.customFilter = function (cb) {
+//   let result = [];
+//   const arr = this;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (cb(arr[i])) {
+//       result.push(arr[i]);
+//     }
+//   }
+//   return result;
+// };
+
+// filter protoype using for each loop
+
+Array.prototype.customFilter = function (cb) {
+  let result = [];
+  const arr = this;
+  arr.forEach((ele) => {
+    if (cb(ele)) {
+      result.push(ele);
+    }
+  });
+  return result;
+};
+
+const customFilteredEle = arr.customFilter(getEven);
+
+console.log("customFilteredEle", customFilteredEle);
+
+// write a function like filter
+
+function customFilters(arr, cb) {
+  let result = [];
+  arr.forEach((ele) => {
+    if (cb(ele)) {
+      result.push(ele);
+    }
+  });
+  return result;
+}
+
+const customFilteredArr = customFilters(arr, getEven);
+
+console.log("customFilteredElem", customFilteredArr);

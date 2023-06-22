b98f60ce8766d728be1248a5a655391fb6074595
Shalu Kumari <16433381+shablcuShaluRai@users.noreply.github.com>
2023-06-22 09:01:59 +0530

    filter polyfill

--- src/jspollyfil/arrFilter.js
+++ src/jspollyfil/arrFilter.js
@@ -8,33 +8,39 @@ console.log("filteredEle", filteredEle);
 
 // filter protoype using for loop
 
-// Array.prototype.customFilter = function (cb) {
-//   let result = [];
-//   const arr = this;
-//   for (let i = 0; i < arr.length; i++) {
-//     if (cb(arr[i])) {
-//       result.push(arr[i]);
-//     }
-//   }
-//   return result;
-// };
-
-// filter protoype using for each loop
-
 Array.prototype.customFilter = function (cb) {
   let result = [];
   const arr = this;
-  arr.forEach((ele) => {
-    if (cb(ele)) {
-      result.push(ele);
+  for (let i = 0; i < arr.length; i++) {
+    if (cb(arr[i])) {
+      result.push(arr[i]);
     }
-  });
+  }
   return result;
 };
-
 const customFilteredEle = arr.customFilter(getEven);
 
 console.log("customFilteredEle", customFilteredEle);
+
+// filter protoype using for each loop
+
+// Note: we should avoid using for each loop in polyfill
+// because some browser does not support.
+
+// Array.prototype.customFilter = function (cb) {
+//   let result = [];
+//   const arr = this;
+//   arr.forEach((ele) => {
+//     if (cb(ele)) {
+//       result.push(ele);
+//     }
+//   });
+//   return result;
+// };
+
+// const customFilteredEle = arr.customFilter(getEven);
+
+// console.log("customFilteredEle", customFilteredEle);
 
 // write a function like filter
 
